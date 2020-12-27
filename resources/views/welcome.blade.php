805acdd60551ce9b8e5caa022382895fe0602cf6
Takishita Noriyuki <n.taki.0416@gmail.com>
2020-12-28 07:55:07 +0900

    トップページのスマートフォンでのレイアウト崩れを修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,70 +11,10 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-                margin-top: 80%;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-
-            #whatsthis {
-                text-align: left
-            }
-
-            h1 {
-                margin-top: 50%;
-            }
-        </style>
+        <link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        <div class="flex-center position-ref">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -94,7 +34,7 @@
                     <h1>このツールでできること</h1>
                     <h2>毎日計測した体重、体脂肪、BMIを記録することができます。</h2>
                     <p>日々の記録の変化がグラフで確認することができます。</p>
-                    <img src="figure1.png"/>
+                    <img id="graph" src="figure1.png"/>
                     <h2>登録・ご利用は無料です。どなたでもご利用いただけます。</h2>
                 </div>
             </div>
